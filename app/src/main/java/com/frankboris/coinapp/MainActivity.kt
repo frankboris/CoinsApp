cdf36742e4038061007612bb252c0d16b427d656
Frank Boris <frankborisdakleu@gmail.com>
2022-05-27 12:14:51 +0400

    configure material theme

--- app/src/main/java/com/frankboris/coinapp/MainActivity.kt
+++ app/src/main/java/com/frankboris/coinapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.frankboris.coinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.frankboris.coinapp.ui.theme.CoinsAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CoinsAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
     }
 }
