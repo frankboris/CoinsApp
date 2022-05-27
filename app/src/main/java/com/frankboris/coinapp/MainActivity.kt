2458098b59df85f6d53cade8929b1bd6c7f12ed8
Frank Boris <frankborisdakleu@gmail.com>
2022-05-27 18:35:18 +0400

    fix: java.lang.NoSuchMethodError: No static method setContent

--- app/src/main/java/com/frankboris/coinapp/MainActivity.kt
+++ app/src/main/java/com/frankboris/coinapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.frankboris.coinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import com.frankboris.coinapp.ui.theme.CoinsAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
